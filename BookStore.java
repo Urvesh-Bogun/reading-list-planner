b8182603d76520629575bfca132a538cc9dd589d
Urvesh Bogun <urveshbogun@gmail.com>
2023-07-15 21:23:24 +0100

    what ive done so far

---
+++ BookStore.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class BookStore extends ReadingListItemStore {
+
+    public BookStore() 
+    {
+        super();
+    }
+
+    public BookStore(String fileName) throws IOException 
+    {
+        super(fileName);
+    }
+
+    @Override
+    public String getRandomItem(String key) 
+    {
+        String item = super.getRandomItem(key);
+        if (item == null)
+        {
+            return null;
+        } else {
+            String[] parts = item.split(" ");
+            String titleAuthor = "";
+            for (int i = 0; i < parts.length - 1; i++) 
+            {
+                titleAuthor += parts[i] + " ";
+            }
+            int year = Integer.parseInt(parts[parts.length - 1]);
+            String type;
+            if (year >= 1990) {
+                type = " (contemporary)";
+            } else if (year >= 1900) {
+                type = " (modern)";
+            } else {
+                type = " (classic)";
+            }
+            return titleAuthor + type;
+        }
+    }
+
+}
---
+++ ReadingListItemStoreTest.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class ReadingListItemStoreTest 
+{
+
+    public static void main(String[] args) 
+    {
+        ReadingListItemStore store = new ReadingListItemStore();
+        store.put("a", "Allen Ginsburg");
+        store.put("be", "Ben Jonson");
+        store.put("be", "Beryl Bainbridge");
+        store.put("a", "Anne Brontë");
+        System.out.println(store.getRandomItem("A"));//"Allen Ginsburg" or "Anne Brontë"
+        System.out.println(store.getRandomItem("a"));//"Allen Ginsburg" or "Anne Brontë"
+        System.out.println(store.getRandomItem("x"));//"null"
+
+        try 
+        {
+            ReadingListItemStore store2 = new ReadingListItemStore("prize-winners.txt");
+            System.out.println(store2.getRandomItem("u"));//starting w/ "u"
+            System.out.println(store2.getRandomItem("r"));//starting w/ "r"
+            System.out.println(store2.getRandomItem("v"));//starting w/ "v"
+            System.out.println(store2.getRandomItem("e"));//starting w/ "e"
+            System.out.println(store2.getRandomItem("s"));//starting w/ "s"
+            System.out.println(store2.getRandomItem("h"));//starting w/ "h"
+        } catch (IOException e) {
+            System.out.println("Error loading prize winners.");
+        }
+
+        try 
+        {
+            ReadingListItemStore store3 = new BookStore("books.txt");
+            System.out.println(store3.getRandomItem("b"));//starting w/ "b" 
+            System.out.println(store3.getRandomItem("o"));//starting w/ "o" 
+            System.out.println(store3.getRandomItem("g"));//starting w/ "g" 
+            System.out.println(store3.getRandomItem("u"));//starting w/ "u" 
+            System.out.println(store3.getRandomItem("n"));//starting w/ "n" 
+        } catch (IOException e) {
+            System.out.println("Error loading books.");
+        }
+    }
+
+}
